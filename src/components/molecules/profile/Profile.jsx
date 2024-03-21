a9faa9582982069df035706fb5c20bea92652299
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-03-21 18:11:56 -0300

    add gsap to control animations and using it in dropdown component for user menu

--- src/components/molecules/profile/Profile.jsx
+++ src/components/molecules/profile/Profile.jsx
@@ -2,8 +2,8 @@ import EditIcon from "../../atoms/edit-icon/EditIcon";
 import Image from "../../atoms/image/Image";
 import styles from './Profile.module.css';
 
-const Profile = ({ imageUrl, hasPermissionToEdit, classList, className }) => (
-    <div className={className}>
+const Profile = ({ imageUrl, hasPermissionToEdit, classList, className, onClick}) => (
+    <div className={className} onClick={onClick}>
       <Image src={imageUrl} alt="" classList={classList} />
       {hasPermissionToEdit && <EditIcon classList={styles.editIcon}/>}
     </div>
--- src/components/organisms/DropdownUserMenu/DropdownUserMenu.jsx
+++ src/components/organisms/DropdownUserMenu/DropdownUserMenu.jsx
@@ -1,34 +1,65 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
-import styles from './DropdownUserMenu.module.css'; // Ajusta el nombre del archivo CSS según corresponda
+import gsap from 'gsap';
+
+import styles from './DropdownUserMenu.module.css'; 
 import Profile from '../../molecules/profile/Profile';
 import defaultProfilePic from '../../../../src/assets/img/defaultProfilePic.jpg';
 
 const DropdownUserMenu = ({ user, username, path }) => {
-    const [isOpen, setIsOpen] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
     const profilePicUrl = user.userInfo ? user.userInfo.profilePicUrl : defaultProfilePic;
-    const handleHover = () => {
-        setIsOpen(true);
+    
+    // Ref para el menú desplegable y para la animación de GSAP
+    const dropdownMenuRef = useRef(null);
+    const menuAnimationRef = useRef(null);
+
+    useEffect(() => {
+        const menuUp = "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"; // Corregido: 0% en la última coordenada Y
+        const menuDown = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)";
+        const menuItems = dropdownMenuRef.current.querySelectorAll("li");
+
+        menuAnimationRef.current = gsap.timeline({
+          paused: true,
+          defaults: { duration: 0.3, ease: "power1.inOut" }
+        })
+          .to(dropdownMenuRef.current, { clipPath: menuUp, visibility: "hidden" }, 0) 
+          .to(dropdownMenuRef.current, { clipPath: menuDown, visibility: "visible" });
+
+        // Animación de los elementos del menú
+        menuAnimationRef.current.fromTo(
+            menuItems,
+            { opacity: 0, y: "0.5em" }, // Configuración inicial
+            { opacity: 1, y: "0em", stagger: 0.1 } // Configuración final
+        );
+    }, []);
+
+    useEffect(() => {
+        if (isOpen) {
+            menuAnimationRef.current.play();
+        } else {
+            menuAnimationRef.current.reverse();
+        }
+    }, [isOpen]);
+
+    const handleOnClick = () => {
+        setIsOpen(prev => !prev);
     };
 
-    const handleLeave = () => {
-        setIsOpen(false);
-    };
     return (
-        <div className={styles.dropdownUserMenu} onMouseEnter={handleHover} onMouseLeave={handleLeave}>
-            <Profile imageUrl={profilePicUrl} className={styles.profilePic}/>
-            {isOpen && (
-                <nav className={styles.dropdownMenu}>
-                    <ul className={styles.navItems}>
-                        <li className={styles.navItem}>
-                            <NavLink className={`${path === `/portfolio/${username}/settings` ? styles.active : ""}`} to={`portfolio/${username}/settings`}>Configuraciones</NavLink>
-                        </li>
-                        <li className={`${styles.navItem} ${styles.btnLogout}`}><NavLink to="/logout">Cerrar Sesión</NavLink></li>
-                    </ul>
-                </nav>
-            )}
+        <div className={styles.dropdownUserMenu}  >
+            <Profile imageUrl={profilePicUrl} className={styles.profilePic} onClick={handleOnClick}/>
+            <nav className={`${styles.dropdownMenu} dropdownMenu`} ref={dropdownMenuRef}>
+                <ul className={`${styles.navItems} navItems`}>
+                    <li className={`${styles.navItem} navItem`}>
+                        <NavLink className={`${path === `/portfolio/${username}/settings` ? styles.active : ""}`} to={`portfolio/${username}/settings`}>Configuraciones</NavLink>
+                    </li>
+                    <li className={`${styles.navItem} ${styles.btnLogout} navItem`}><NavLink to="/logout">Cerrar Sesión</NavLink></li>
+                </ul>
+            </nav>
         </div>
     );
 };
 
 export default DropdownUserMenu;
+
