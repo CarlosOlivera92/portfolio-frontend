1703797f590122f26448242700604cebff3c76a9
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-03-22 18:28:33 -0300

    add dragable attr to images component

--- src/components/atoms/image/Image.jsx
+++ src/components/atoms/image/Image.jsx
@@ -2,7 +2,7 @@ import styles from './Image.module.css';
 const Image = ({src,alt,classList}) => {
     return (
         <div className={styles.image}>
-            <img src={src} alt={alt} className={classList} />
+            <img src={src} alt={alt} className={classList} draggable={false}/>
         </div>
     )
 } 
